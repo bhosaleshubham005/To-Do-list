28df2c8452dc20cdda73234b818473485f9d8964
bhosaleshubham005 <bhosaleshubham575@gmail.com>
2026-07-31 13:00:51 +0530

    added useState

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,10 +1,39 @@
-function app ()
-{
-  return(
-    <div>
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (task.trim() === "") return;
+
+    setTodos([...todos, task]);
+    setTask("");
+  };
+
+  return (
+    <div className="container">
       <h1>To-Do List</h1>
+
+      <div className="input-area">
+        <input
+          type="text"
+          placeholder="Enter the task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+
+        <button onClick={addTodo}>Add</button>
+      </div>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default app;
+export default App;
